a64132b4ceea8bb8beec3a90c11010db0a366e5c
Inho Choi <55151796+E-know@users.noreply.github.com>
2022-10-15 14:04:39 +0900

    Initial commit

---
+++ WeHere/WeHereApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeHereApp.swift
+//  WeHere
+//
+//  Created by Inho Choi on 2022/10/15.
+//
+
+import SwiftUI
+
+@main
+struct WeHereApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
