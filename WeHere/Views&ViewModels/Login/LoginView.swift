eff0ff91f8652e6bd708bb374edca1914047b2a2
Inho Choi <eknow25@kakao.com>
2022-11-20 02:49:33 +0900

    🍱 Add LoginView Asset

---
+++ WeHere/Views&ViewModels/Login/LoginView.swift
@@ -0,0 +1,20 @@
+//
+//  LoginView.swift
+//  WeHere
+//
+//  Created by Inho Choi on 2022/11/20.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
