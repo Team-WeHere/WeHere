5118d7b84967b77363ffc0dcd26ca7fcbf25b97a
Inho Choi <eknow25@kakao.com>
2022-11-21 21:52:51 +0900

    :bento: Modify Font

--- WeHere/Extensions/Font+.swift
+++ WeHere/Extensions/Font+.swift
@@ -14,13 +14,13 @@ extension Font {
 struct FontTheme {
     fileprivate init () { }
     
-    let largeTitle = Font.system(size: 34).weight(.bold)
-    let title1 = Font.system(size: 28).weight(.semibold)
-    let title2 = Font.system(size: 20).weight(.semibold)
-    let headline = Font.system(size: 17).weight(.bold)
-    let body1 = Font.system(size: 17).weight(.regular)
-    let body2 = Font.system(size: 15).weight(.regular)
-    let callout = Font.system(size: 16).weight(.semibold)
-    let subhead = Font.system(size: 16).weight(.semibold)
-    let footnote = Font.system(size: 13).weight(.regular)
+    let largeTitle = Font.system(size: 34, weight: .bold)
+    let title1 = Font.system(size: 24, weight: .semibold)
+    let title2 = Font.system(size: 22, weight: .semibold)
+    let headline = Font.system(size: 17, weight: .bold)
+    let body1 = Font.system(size: 17, weight: .regular)
+    let body2 = Font.system(size: 15, weight: .regular)
+    let callout = Font.system(size: 16, weight: .semibold)
+    let subhead = Font.system(size: 15, weight: .semibold)
+    let footnote = Font.system(size: 13, weight: .regular)
 }
