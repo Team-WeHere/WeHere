8d7918e002516580353f9fe799c36c03335ac4d0
Inho Choi <eknow25@kakao.com>
2022-11-28 05:14:50 +0900

    :sparkles: Add EditProfileView

--- WeHere/Views&ViewModels/Main/Profile/EditProfileView.swift
+++ WeHere/Views&ViewModels/Main/Profile/EditProfileView.swift
@@ -7,19 +7,45 @@
 
 import SwiftUI
 
-struct EditProfile: View {
+struct EditProfileView: View {
     @Binding var showModal: Bool
     @Binding var userName: String
+    @Binding var image: String
     
+    @State var isChange = false
+    @State var showAlert = false
     
     var body: some View {
         VStack(alignment: .leading) {
-            // Title & X 버튼
+            // MARK: Title & X 버튼
             ZStack {
+                HStack {
+                    Spacer()
+                    Text("프로필 정보 수정")
+                    Spacer()
+                }
+                .padding(.bottom, 8)
                 
+                HStack {
+                    Spacer()
+                    Button(action: {
+                        showModal.toggle()
+                    }, label: {
+                        Image(systemName: "x.circle.fill")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .foregroundColor(.gray)
+                            .frame(width: 30, height: 30)
+                    })
+                }
             }
             
+            // MARK: 닉네임
             Text("닉네임")
+                .font(.theme.headline)
+                .foregroundColor(.gray02)
+                .padding(.top, 36)
+            
             HStack {
                 TextField("", text: $userName)
                     .onChange(of: userName) { _ in
@@ -27,22 +53,81 @@ struct EditProfile: View {
                             let endIndex = userName.index(userName.startIndex, offsetBy: 8)
                             userName = String(userName[userName.startIndex..<endIndex])
                         }
+                        isChange = true
                     }
+                    .font(.theme.body1)
+                    .foregroundColor(.gray01)
+                    .padding(.leading, 10)
                 
                 Text("\(userName.count) / 8")
+                    .foregroundColor(.gray02)
+                    .font(.theme.body2)
             }
+            .padding(.top, 21)
             
+            Divider()
+                .padding(.top, 11)
+            
+            // MARK: 프로필 사진
             Text("프로필 사진")
+                .foregroundColor(.gray02)
+                .font(.theme.headline)
+                .padding(.top, 30)
             
-            HStack {
+            HStack(spacing: 40) {
+                Button(action: {
+                    image = "woman"
+                    isChange = true
+                }, label: {
+                    Image("woman")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                })
+                .frame(width: 81, height: 81)
                 
+                Button(action: {
+                    image = "man"
+                    isChange = true
+                }, label: {
+                    Image("man")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+    
+                })
+                .frame(width: 81, height: 81)
             }
+            .padding(.top, 20)
+            
+            Spacer()
+            
+            // MARK: 버튼
+            Button(action: {
+                showAlert.toggle()
+            }, label: {
+                ZStack {
+                    Rectangle()
+                        .foregroundColor(isChange ? .pink02 : .gray05)
+                        .cornerRadius(30)
+                        .frame(width: UIScreen.main.bounds.width - 32, height: 56)
+                    
+                    Text("변경하기")
+                        .font(.theme.body1)
+                        .foregroundColor(isChange ? .white : .gray03)
+                }
+            })
+            .padding(.bottom, 20)
         }
-    }
-}
-
-struct EditProfile_Previews: PreviewProvider {
-    static var previews: some View {
-        EditProfile(showModal: .constant(true), userName: .constant("우리요기"))
+        .padding(.horizontal, 16)
+        .padding(.top, 16)
+        .alert(isPresented: $showAlert, content: {
+            Alert(title: Text("알림"),
+                  message: Text("프로필을 수정할까요?"),
+                  primaryButton: .default(Text("네")) {
+                showModal.toggle()
+            },
+                  secondaryButton: .default(Text("아니오")) {
+                showAlert.toggle()
+            })
+        })
     }
 }
--- WeHere/Views&ViewModels/Main/Profile/ProfileView.swift
+++ WeHere/Views&ViewModels/Main/Profile/ProfileView.swift
@@ -11,6 +11,9 @@ struct ProfileView: View {
     @State var countDay: Int = 508 // TODO: Firebase 연동 @Toby
     @State var partnerName = "푸린"
     @State var countCock = 1508
+    @State var showModal = false
+    @State var userName = "우리요기"
+    @State var image = "man"
     var body: some View {
         VStack {
              Text("연결한지 \(countDay)일이 지났어요!")
@@ -25,6 +28,12 @@ struct ProfileView: View {
             
             HStack(spacing: 20) {
                 profileImage()
+                    .onTapGesture {
+                        showModal.toggle()
+                    }
+                    .sheet(isPresented: $showModal) {
+                        EditProfileView(showModal: $showModal, userName: $userName, image: $image)
+                    }
                 profileImage() 
             }
             .padding(.horizontal, 16)
@@ -36,10 +45,14 @@ struct ProfileView: View {
 
 private extension ProfileView {
     func profileImage() -> some View { // 추후에 매개변수로 이미지를 불러올 것이기에 함수형으로 선언하였습니다.
-        Rectangle()
-            .foregroundColor(.gray)
-            .cornerRadius(20)
-            .frame(width: 169, height: 171)
+        ZStack {
+            RoundedRectangle(cornerRadius: 20)
+                .stroke(.gray)
+                .frame(width: 169, height: 171)
+            
+            HeartView()
+                .frame(width: 80, height: 75)
+        }
     }
 }
 
